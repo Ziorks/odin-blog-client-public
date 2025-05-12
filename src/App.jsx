58aec928b3282d4083caaa6cc4be6370cfa4a63c
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2025-05-12 13:36:46 -0500

    add store user in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   const [user, setUser] = useState(null);
-
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      setUser(JSON.parse(user));
+    }
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
     setUser(null);
   };
 
   const handleLogin = ({ user, token }) => {
     localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(user));
     setUser(user);
     navigate("/", { replace: true });
   };
