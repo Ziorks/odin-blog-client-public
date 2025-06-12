7c3d7f259190c3bbf0a6b23dc147dd5c668053c4
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2025-06-11 20:06:02 -0500

    add styling to error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
 import styles from "./Error.module.css";
 
 function Error() {
   return (
-    <main>
-      <h1 className={styles.heading}>Error Page</h1>;
+    <main className={styles.mainContainer}>
+      <h1 className={styles.header}>
+        404 <br />
+        Page Not Found
+      </h1>
+      <div className={styles.linkContainer}>
+        <Link to={-1}>Go Back</Link>
+        <Link to="/">Home</Link>
+      </div>
     </main>
   );
 }
